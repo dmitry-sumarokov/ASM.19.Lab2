d5159fb45bdc04c37ff5a316ab6d54a813befc1c
Ivana Begovic <ivana.begovic@crypterium.com>
2019-11-14 19:57:59 +0300

    added css

--- asm1905/st21/main.py
+++ asm1905/st21/main.py
@@ -1,16 +1,93 @@
-from flask import Flask
+from flask import Flask, url_for, render_template
+import cgi
+import cgitb
+from strategy.menuItems import ADDITION_MENU
 
+cgitb.enable()
 app = Flask(__name__)
 
 if __name__ == '__main__':
-    from group import group
+    from strategy.main import scrumTeam
 else:
-    from .group import group
+    from .strategy.main import scrumTeam
 
 
 @app.route("/")
 def main():
-    return group().f()
+    scrumTeam.load_from_file()
+    employees_num = enumerate(scrumTeam.employees)
+    return render_template('index.html', menu=ADDITION_MENU, employees=employees_num)
+
+
+@app.route('/team/add', defaults={'number': None})
+@app.route('/team/add/<int:number>', methods=['GET', 'POST'])
+def add_student(number):
+    form = cgi.FieldStorage()
+    text1 = form.getfirst("TEXT_1", "не задано")
+    text2 = form.getfirst("TEXT_2", "не задано")
+
+    if number is None:
+        return render_template('add.html')
+    else:
+        templ_name = ['student', 'starosta', 'proforg']
+        return render_template('addition.html', number=number)
+
+    # if request.method == 'POST':
+    #     data = request.form
+    #     myG.add_student(number, data=data, type_context='web')
+    #     return render_template("menu.html")
+
+
+@app.route('/team/edit', defaults={'number': None})
+@app.route('/team/edit/<int:number>', methods=['GET', 'POST'])
+def change_student(number):
+    if request.method == 'POST':
+        data = request.form
+        myG.change_student(number, data=data)
+        return render_template("menu.html")
+    if number is None:
+        return render_template('edit.html', length=myG.amount_students)
+    else:
+        return myG.change_student(number, data={'web': number})
+
+
+@app.route('/team/show')
+def print_group():
+    page_group = render_template('show_top.html', length=myG.amount_students)
+    body = myG.print_group()
+    if body is not None:
+        page_group += body
+    page_group += render_template('show_f.html')
+    return page_group
+
+
+@app.route('/team/remove/<int:number>', methods=['GET', 'POST'])
+def remove_student():
+    if request.method == 'GET':
+        return render_template('remove.html', length=myG.amount_students - 1)
+    else:
+        if request.form['number']:
+            number = int(request.form['number'])
+            myG.remove_student(number)
+        return render_template('menu.html')
+
+
+@app.route('/team/magic', defaults={'number': None})
+@app.route('/team/magic/<int:number>')
+def do_magic(number):
+    return render_template('magic.html', length=myG.amount_students) if number is None else myG.do_magic(number)
+
+
+@app.route('/team/save')
+def save_to_file():
+    myG.save_to_file()
+    return render_template('menu.html')
+
+
+@app.route('/team/load')
+def load_from_file():
+    myG.load_from_file()
+    return render_template('menu.html')
 
 
 if __name__ == "__main__":
--- asm1905/st21/strategy/scrumTeam.py
+++ asm1905/st21/strategy/scrumTeam.py
@@ -8,30 +8,30 @@ from .commonFunctions import return_number
 
 
 class ScrumTeam:
-    __slots__ = ('lala', '_employees')
+    __slots__ = ('lala', 'employees')
 
     def __init__(self):
-        self._employees = []
+        self.employees = []
 
     def add_employee(self):
         e = ADDITION_MENU[int(get_menu_input(ADDITION_MENU))]
-        self._employees.append(Employee(e[1], e[2]))
+        self.employees.append(Employee(e[1], e[2]))
 
     def edit_employee(self):
-        self._employees[return_number()].edit()
+        self.employees[return_number()].edit()
         print(Fore.YELLOW + 'Saved!')
 
     def remove_employee(self):
-        self._employees.pop(return_number())
+        self.employees.pop(return_number())
         print(Fore.YELLOW + 'Deleted!')
 
     def show_scrum_team(self):
-        for i, employee in enumerate(self._employees):
+        for i, employee in enumerate(self.employees):
             print(Fore.MAGENTA + '~Employee №' + str(i) + '~')
             employee.print_data()
 
     def listen_personal(self):
-        self._employees[return_number()].saying()
+        self.employees[return_number()].saying()
 
     # The pickle module implements binary protocols for serializing and de-serializing a Python object structure.
     # “Pickling” is the process whereby a Python object hierarchy is converted into a byte stream, and “unpickling”
@@ -48,17 +48,15 @@ class ScrumTeam:
             file = open(os.path.join(os.path.abspath(__name__).replace('.st21.scrumTeam', '/st21'),
                                      fn
                                      ), 'xb')
-        pickle.dump(self._employees, file)
+        pickle.dump(self.employees, file)
         print(Fore.MAGENTA + 'Saved to "' + fn + '" file!')
 
     def load_from_file(self):
-        fn = input(Fore.CYAN + 'Enter file name: ')
+        # fn = input(Fore.CYAN + 'Enter file name: ')
         try:
-            file = open(
-                os.path.join(os.path.abspath(__name__).replace('.st21.scrumTeam', '/st21'),
-                             fn
-                             ), 'rb')
-            self._employees = pickle.load(file)
-            print(Fore.MAGENTA + 'Loaded from "' + fn + '" file!')
+            print(os.path.abspath(__name__))
+            file = open('afaf', 'rb')
+            self.employees = pickle.load(file)
+            # print(Fore.MAGENTA + 'Loaded from "' + fn + '" file!')
         except IOError:
             print(Fore.MAGENTA + 'File doesn`t exist!')
