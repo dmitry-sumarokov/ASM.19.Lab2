267f76678bbf9187f8557ccd48540d0cc93a2d49
ivan <tanin-ivan@mail.ru>
2019-11-22 09:49:55 +0300

    fix(asm1904/st19/): глобальные правки

--- asm1904/st19/main.py
+++ asm1904/st19/main.py
@@ -1,15 +1,42 @@
+# from group import Group
 from flask import Flask
+from flask import g
+from StudentAbstract import Group
+
 app = Flask(__name__)
 
-if __name__ == '__main__':
-    from group import group
-else:
-    from .group import group
+group = Group()
+
 
 @app.route("/")
-def main():
-	return group().f()
+def index():
+    return group.printHeader() + group.ShowGroup()
 
+@app.route("/ShowForm/<int:id>/<int:kind>")
+def ShowForm(id, kind):
+	return group.printHeader() + group.ShowForm(id, kind)
+
+@app.route("/New", methods=['POST'])
+def AddStudent():
+	return group.printHeader() + group.AddStudent() + group.PrintFooter()
+
+@app.route("/DeleteStudent/<int:id>/")
+def DeleteStudent(id):
+	return group.printHeader() + group.DeleteStudent(id) + group.PrintFooter()
+
+@app.route("/DoSpecialAction/<int:id>/")
+def DoSpecialAction(id):
+	return group.DoSpecialAction(id) + group.PrintFooter()\
+
+@app.route("/EditStudent/<int:id>/<int:kind>")
+def EditStudent(id, kind):
+	return group.EditStudent(id, kind) + group.PrintFooter()
+
+@app.teardown_appcontext
+def finish(ctx):
+        group.store()
 
 if __name__ == "__main__":
-	app.run(debug=True)
+    app.run(debug=True)
+
+
