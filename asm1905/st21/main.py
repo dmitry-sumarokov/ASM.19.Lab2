6537726123933d9b1def36a3c49d0c1030f2f005
Ivana Begovic <ivana.begovic@crypterium.com>
2020-01-13 16:33:08 +0300

    removed wtforms form project

--- asm1905/st21/main.py
+++ asm1905/st21/main.py
@@ -1,28 +1,18 @@
 import os
 
 from flask import Flask, render_template, request, redirect
-from flask_wtf import FlaskForm
-from flask_wtf.csrf import CSRFProtect
 from strategy.employee import Employee
 from strategy.menuItems import ADDITION_MENU, MENU
 from strategy.scrumTeam import ScrumTeam
-from wtforms import StringField
-from wtforms.validators import DataRequired
 
 SECRET_KEY = os.urandom(32)
-csrf = CSRFProtect()
 app = Flask(__name__)
 app.config['SECRET_KEY'] = SECRET_KEY
-csrf.init_app(app)
 scrumTeam = ScrumTeam()
 
 
 def render_header():
     return render_template('boilerplate.html')
-
-
-class AdditionForm(FlaskForm):
-    pass
 
 
 def get_form_data(number):
@@ -37,54 +27,49 @@ def get_form_data(number):
 @app.route('/')
 def main():
     employees_num = enumerate(scrumTeam.employees)
-    return render_header() + render_template('index.html', length=len(scrumTeam.employees), menu=MENU,
-                                             employees=employees_num)
+    return _add_boilerplate(
+        render_template('index.html', length=len(scrumTeam.employees), menu=MENU, employees=employees_num))
 
 
 @app.route('/add/', defaults={'number': None})
 @app.route('/add/<int:number>', methods=['GET', 'POST'])
 def add_student(number):
     if number is None:
-        return render_header() + render_template('addition.html', menu=ADDITION_MENU)
+        return _add_boilerplate(render_template('addition.html', menu=ADDITION_MENU))
 
     if request.method == 'POST':
-        form = AdditionForm()
-        if form.validate_on_submit():
+        if not _get_form_fields():
+            form = _create_form(number)
+            return _add_boilerplate(render_template('addition.html', form=form, number=number, endpoint='add',
+                                                    msg='All fields are required!'))
+        else:
             data = get_form_data(number)
             scrumTeam.add_employee(data)
             return redirect('/')
-        return redirect('/add/')
 
     else:
-        fields = scrumTeam.get_fields(number)
-        for field in fields:
-            setattr(AdditionForm, field, StringField(validators=[DataRequired()]))
-        form = AdditionForm()
-        for field in fields:
-            delattr(AdditionForm, field)
-        return render_header() + render_template('addition.html', form=form, number=number, endpoint='add',
-                                                 fields=fields)
+        form = _create_form(number)
+        return _add_boilerplate(render_template('addition.html', form=form, number=number, endpoint='add'))
 
 
 @app.route('/edit/<int:number>', methods=['GET', 'POST'])
 def change_student(number):
+    employee_data = scrumTeam.employees[number]
+    form = _create_form(number, employee_data)
+
     if request.method == 'POST':
-        form = AdditionForm()
-        if form.validate_on_submit():
+        if not _get_form_fields():
+            return _add_boilerplate(
+                render_template('addition.html', form=form, number=number, endpoint='edit',
+                                msg='All fields are required!'))
+        else:
             data = get_form_data(number)
             scrumTeam.edit_employee(data, number)
             return redirect('/')
 
     elif number is not None:
-        employee_data = scrumTeam.employees[number]
-        fields = scrumTeam.get_fields(number)
-        for field in fields:
-            setattr(AdditionForm, field, StringField(default=employee_data.get(field), validators=[DataRequired()]))
-        form = AdditionForm()
-        for field in fields:
-            delattr(AdditionForm, field)
-        return render_header() + render_template('addition.html', form=form, number=number, endpoint='edit',
-                                                 fields=fields)
+        return _add_boilerplate(
+            render_template('addition.html', form=form, number=number, endpoint='edit'))
 
 
 @app.route('/remove/<int:number>', methods=['GET', 'POST'])
@@ -104,6 +89,44 @@ def save_to_file():
 def load_from_file():
     scrumTeam.load_from_file()
     return redirect('/')
+
+
+def _create_form(number, values=None) -> str:
+    form = ''
+    fields = scrumTeam.get_fields(number)
+    for field in fields:
+        if values:
+            form += _make_field(field, values.get(field.lower()))
+        else:
+            form += _make_field(field)
+    return form
+
+
+def _make_field(name, value=None) -> str:
+    dt = "<dt>" + name.title() + ":"
+    dt_end = "</dt>"
+    input_start = '<input name='
+    input_end = '> </input>'
+    if value:
+        temp = ' value=' + str(value) + input_end
+    else:
+        temp = input_end
+    return dt + input_start + name.lower() + temp + dt_end
+
+
+def _add_boilerplate(template) -> str:
+    return render_header() + template
+
+
+def _get_form_fields() -> list or bool:
+    answers = []
+    form = request.form
+    for i in form.keys():
+        if form.get(i) == '':
+            return False
+        else:
+            answers.append(form.get(i))
+    return answers
 
 
 if __name__ == '__main__':
